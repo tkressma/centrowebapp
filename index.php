98ecdc799b9ff3e4861e4d3a045f1a992fc29c16
tkressma <58404684+tkressma@users.noreply.github.com>
2019-12-02 20:04:56 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -9,34 +9,80 @@
         else 
             header("Location: welcome.php");
     }
+	include('head.php');
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Centro Bus Web Application - Login</title>
-	<link rel="stylesheet" href="bootstrap/css/bootstrap1.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: white;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+/* Add padding to containers */
+.container {
+  padding: 16px;
+  background-color: #f1f1f1;
+}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: white;
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  background-color: white;
+  outline: none;
+}
+
+/* Overwrite default styles of hr */
+hr {
+  border: 1px solid #0071C1;
+  margin-bottom: 25px;
+}
+
+/* Set a style for the submit button */
+.registerbtn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+.registerbtn:hover {
+  opacity: 1;
+}
+
+/* Add a blue text color to links */
+a {
+  color: dodgerblue;
+}
+
+/* Set a grey background color and center the text of the "sign in" section */
+.signin {
+  background-color: #0071C1;
+  text-align: center;
+}
+</style>
 </head>
-
-
-<body>
-
-<div class="container">
-	<h1 align="center"><b>Centro Bus Web App</b></h1><hr>
-	<h2 align="center">Login</h2><hr>
-	<form class="form-horizontal" method="POST">
-		<div class="form-group" style="padding: 10px 10px 10px 10px;">
-			<label for="user_name">Username:</label>
-			<input type="text" name="user_name"  class="form-control" id="inputID" placeholder="Type your username." 
-				required autocomplete="off" readonly onfocus="this.removeAttribute('readonly');" />
-		</div>
-		<div class="form-group" style="padding: 10px 10px 10px 10px;">
-			<label for="user_password">Password:</label>
-			<input type="password" name="user_password" class="form-control" id="inputPassword" placeholder="Type your password." 
-				required  autocomplete="off" readonly onfocus="this.removeAttribute('readonly');" />
-		</div>
-
-
+<body class="text-center">
 <?php
 
     $login_ok = false;
@@ -99,7 +145,7 @@
 					else
 						header('location:welcome.php');
 					session_write_close();
-			} else{
+			} else {
 			?>
 <div class="alert alert-danger">
   <strong>Login Failed.</strong> Username or password is incorrect.
@@ -108,16 +154,34 @@
 		}
 	}
 ?>
-		<div class="checkbox">
-			<label><input type="checkbox">Remember Username</label>
-		</div>
-		</br>
-		<div class="from-group" style="padding: 10px 10px 10px 10px;" >
-			<button type="submit" name="login" class="btn btn-success" <span class="glyphicon glyphicon-log-in"></span>Login</button>
-			<a class="btn btn-success" href="registration.php" style="margin-left: 50px">
-			<span class="glyphicon glyphicon-user"></span>&nbsp;Register</a>
-		</div>
-		</br>
-	</form>
+<form id="form" method="post" enctype="multipart/form-data">
+  <div class="container">
+    <h1><b>Centro Bus Web Application</b></h1>
+    <p>Please login to continue.<p>
+    <hr>
+	
+	<label for="user_name">Username:</label>
+	<input type="text" name="user_name"  class="form-control" id="inputID" placeholder="Type your username." 
+	required autocomplete="off" readonly onfocus="this.removeAttribute('readonly');" />
+
+	<label for="user_password">Password:</label>
+	<input type="password" name="user_password" class="form-control" id="inputPassword" placeholder="Type your password." 
+	required  autocomplete="off" readonly onfocus="this.removeAttribute('readonly');" />
+
+    <hr>
+	
+	<div class="checkbox">
+		<label><input type="checkbox">Remember Username</label>
+	</div>
+
+    <button type="submit" name="login" class="btn btn-primary" <span class="glyphicon glyphicon-log-in"></span>Login</button>
+	
+  </div>
+  
+  <div class="container signin">
+    <p>Don't have an account? <a href="registration.php">Click here to register</a>!</p>
+  </div>
+</form>
+
 </body>
 </html>
